dd4464b8d5f7725cbc88d405cf8cd26769910d09
Daisy Watt <daisyalexandrawatt@gmail.com>
2022-03-17 12:53:45 +1100

    adding in a show view

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,11 @@
 module ListingsHelper
+    # def format_condition(condition)
+    #     arr = condition.split("_").map {|word| word.capitalize}.join(" ")
+    # end
+
+    # taking out underscore and capitalizing first letter of words
+
+    def format_price(price)
+        "$#{price/ 100}"
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get "/restricted", to: "pages#restricted"
-  get "/listings", to: "listings#index" 
+  get "/listings", to: "listings#index", as: "listings"
+  get "listings/:id", to: "listings#show", as: "listing"
 end
